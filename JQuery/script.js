64ef051449233c22f8182568352593e541be74a8
Sugam Karki <sugamkarki7058@gmail.com>
2020-09-02 12:13:35 +0545

    sugam

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,1 +1,13 @@
-$("h1").css("color","red")
+// $("p").addClass("beeg");
+$("button#btn1").on("click", large);
+function large() {
+  $("#theText").html("<textarea>yoomu</textarea>");
+  $("#theText").css("background-color",("blue"));
+
+}
+$("button#btn2").on("click",small);
+function small()
+{
+$("").removeClass("beeg");
+
+}
